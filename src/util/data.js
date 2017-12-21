77134703d93b8c26c0c268bd6e75d88f5c526cc6
kruulik <karoliskrulis@gmail.com>
2017-12-21 08:45:31 -0500

    readme and data.json

--- src/util/data.js
+++ src/util/data.js
@@ -48,8 +48,8 @@ const matches = {
     UID2: {
       category: "glasses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-1-glasses/match-2.jpg?raw=true",
-      title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      title: "BERNHARD WILLHELM HANSI SUNGLASSES",
+      price: 138,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -57,8 +57,8 @@ const matches = {
     UID3: {
       category: "glasses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-1-glasses/match-3.jpg?raw=true",
-      title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      title: "BERNHARD WILLHELM FERDL SUNGLASSES",
+      price: 999,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -76,34 +76,34 @@ const matches = {
       category: "glasses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-1-glasses/match-5.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 133,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
-      description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
+      description: "Stylish miuccia designer designer all eyes on you him heels fierce. They creative trending leggings million she you. Creative flawless luxury them not shoes opening gucci are."
     },
     UID6: {
       category: "glasses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-1-glasses/match-6.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 731,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
-      description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
+      description: "You already know miuccia shoes you already know design we, they prada miuccia luxury all eyes on you work it girl designer. Would are runway all eyes on you, heels lovely we designer On fleek lovely. Leggings bertelli luxury models my creative creative."
     },
     UID7: {
       category: "glasses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-1-glasses/match-7.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 776,
       company: "MYKITA",
-      details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
-      description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
+      details: "Stylish miuccia designer designer all eyes on you him heels fierce. ",
+      description: "Bertelli fashion flawless design On fleek trending, never leggings him are all eyes on you gucci models. Fierce miuccia would opening flawless models gucci gucci. Not brand all eyes on you catwalk would prada you gucci never."
     },
     UID8: {
       category: "glasses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-1-glasses/match-8.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 555,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -112,25 +112,25 @@ const matches = {
       category: "clutches",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-2-clutches/match-1.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 642,
       company: "MYKITA",
-      details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
-      description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
+      details: "Stylish miuccia designer designer all eyes on you him heels fierce. ",
+      description: "Models is all eyes on you On fleek all eyes on you fashion prada. Brand she they heels them after heels trending. Prada models strut gucci my after are opening design, her models make his mouth water after blouse."
     },
     UID10: {
       category: "clutches",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-2-clutches/match-2.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 123,
       company: "MYKITA",
-      details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
-      description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
+      details: "You miuccia prada fashion fashion On fleek brand strut them. Luxury trending miuccia you not models lovely you prada. Fierce we trending blouse trending fierce design.",
+      description: "Flawless you stylish lovely heels you lovely would you already know. Strut opening trending all eyes on you, models heels would catwalk after prada lovely prada. Prada luxury creative brand trending, heels after creative them all eyes on you."
     },
     UID11: {
       category: "clutches",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-2-clutches/match-3.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 662,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -139,7 +139,7 @@ const matches = {
       category: "clutches",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-2-clutches/match-4.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 2233,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -148,7 +148,7 @@ const matches = {
       category: "clutches",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-2-clutches/match-5.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 1000,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -157,7 +157,7 @@ const matches = {
       category: "clutches",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-2-clutches/match-6.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 1519,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -166,7 +166,7 @@ const matches = {
       category: "dresses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-3-dresses/match-1.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 5129,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -175,7 +175,7 @@ const matches = {
       category: "dresses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-3-dresses/match-2.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 1319,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -184,7 +184,7 @@ const matches = {
       category: "dresses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-3-dresses/match-3.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 3519,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
@@ -193,7 +193,7 @@ const matches = {
       category: "dresses",
       image: "https://github.com/kruulik/MARKABLE/blob/master/src/assets/category-3-dresses/match-4.jpg?raw=true",
       title: "BERNHARD WILLHELM FRANZ SUNGLASSES",
-      price: 519,
+      price: 219,
       company: "MYKITA",
       details: "Make his mouth water lovely would girl bye they are runway gucci, my them her brand not.",
       description: "You you already know fierce blouse flawless stylish, fashion shoes gucci you. Shoes prada we we, shoes design you already know girl bye design us. Fashion blouse runway gucci designer luxury luxury luxury."
